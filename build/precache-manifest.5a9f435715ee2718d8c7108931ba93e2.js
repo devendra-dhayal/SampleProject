3529154f73af4197b9e3a8a0660446ea90e67fc5
Devendra Dhayal <devendra.dhayal@wholesalebox.co>
2020-04-01 20:13:02 +0530

    added build

---
+++ build/precache-manifest.5a9f435715ee2718d8c7108931ba93e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fc45caa7234151eb9fd1a33ddbb9840",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9933b63e0e02eeea05b",
+    "url": "/static/css/main.64bfaee4.chunk.css"
+  },
+  {
+    "revision": "8852a4bac9323524497f",
+    "url": "/static/js/2.d3a8e349.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.d3a8e349.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9933b63e0e02eeea05b",
+    "url": "/static/js/main.0377d22c.chunk.js"
+  },
+  {
+    "revision": "52321f33498633f449d7",
+    "url": "/static/js/runtime-main.2ab21936.js"
+  }
+]);
